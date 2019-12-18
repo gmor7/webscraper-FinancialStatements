20651812c4a4d1f9ea4eac22e35ee969941fcc75
gmor7 <57103643+gmor7@users.noreply.github.com>
2019-12-18 16:22:08 +0000

    Add files via upload

---
+++ advancedwebscraper.py
@@ -0,0 +1,29 @@
+
+# total debt/total liabilities
+# current assets
+# current liabilities
+# EPS for last five years
+# market cap
+# book value
+
+
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import csv
+
+header = []
+url = 'https://www.hl.co.uk/shares/shares-search-results/h/hochschild-mining-plc-ordinary-25p/financial-statements-and-reports'
+html = requests.get(url).text
+soup = BeautifulSoup(html, "lxml")
+table = soup.find("table")
+print(table)
+
+with open('prices.csv', 'w', newline='') as f_output:
+    csv_output = csv.writer(f_output)
+    csv_output.writerow(header)
+
+    for tr in table.find_all('tr'):
+        row = [td.get_text(strip=True) for td in tr.find_all('td')]
+        csv_output.writerow(row)
+
